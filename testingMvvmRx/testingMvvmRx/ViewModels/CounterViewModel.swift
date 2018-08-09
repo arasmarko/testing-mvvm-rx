adbabb083c900e5b7ba7fe1edd20c67ce013fdb7
arasmarko <marko@arsfutura.co>
2018-08-09 11:54:02 +0200

    UI

---
+++ testingMvvmRx/testingMvvmRx/ViewModels/CounterViewModel.swift
@@ -0,0 +1,33 @@
+//
+//  CounterViewModel.swift
+//  testingMvvmRx
+//
+//  Created by Marko Aras on 09/08/2018.
+//  Copyright © 2018 arsfutura. All rights reserved.
+//
+
+import RxSwift
+
+protocol CounterViewModelProtocol {
+    var increaseButtonTaps: PublishSubject<Void> { get }
+    var counterValue: Observable<String>! { get }
+}
+
+class CounterViewModel: CounterViewModelProtocol {
+    private var counter: Int = 0
+
+    var increaseButtonTaps = PublishSubject<Void>()
+    var counterValue: Observable<String>!
+
+    init() {
+        counterValue = increaseButtonTaps
+            .do(onNext: { [weak self] (_) in
+                self?.counter += 1
+            })
+            .flatMap({ [weak self] (_) -> Observable<String> in
+                guard let `self` = self else { return Observable.empty() }
+                return Observable.just("\(self.counter)")
+            })
+            .startWith("\(counter)")
+    }
+}
